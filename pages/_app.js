f5448d909ae96247275fcd4a8334bedd9ce481df
jaosaari <jaosaari@gmail.com>
2021-05-06 14:53:45 +0200

    commented out nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,16 @@
 /* eslint-disable react/jsx-props-no-spreading */
 
-import '../components/styles/nprogress.css';
+// import '../components/styles/nprogress.css';
 
-import Router from 'next/router';
-import NProgress from 'nprogress';
+// import Router from 'next/router';
+// import NProgress from 'nprogress';
 import PropTypes from 'prop-types';
 
 import Page from '../components/Page';
 
-Router.events.on('routeChangeStart', () => NProgress.start());
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
+// Router.events.on('routeChangeStart', () => NProgress.start());
+// Router.events.on('routeChangeComplete', () => NProgress.done());
+// Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
